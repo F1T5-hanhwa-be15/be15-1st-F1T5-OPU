1bc871934c61be8b0225df94b378afccc3aec362
meowdule <meowdule73@gmail.com>
2025-02-14 08:55:02 +0900

    커뮤니티 (게시글 등등) 테스트케이스 이미지 첨부

--- sql/community/free_board.sql
+++ sql/community/free_board.sql
@@ -1,3 +1,8 @@
+UPDATE post
+   SET is_delete = 'N'
+ WHERE is_delete = 'Y';
+
+
 -- 자유게시판
 
 -- 1. 게시글 목록 조회
@@ -11,7 +16,7 @@ FROM post p
 JOIN post_category c ON p.category_id = c.category_id
 JOIN user u ON p.user_code = u.user_code
 WHERE c.category_name = '자유게시판'
-	AND p.is_delete = 'Y'
+	AND p.is_delete = 'N'
 ORDER BY COALESCE(p.update_at, p.create_at) DESC;
 
 -- 2. 팔로잉 우선 조회 
@@ -31,7 +36,7 @@ JOIN user u ON p.user_code = u.user_code
 LEFT JOIN follow f ON p.user_code = f.following_code 
     AND f.follower_code = 5  -- 사용자 코드 테스트용
 WHERE c.category_name = '자유게시판'
-	AND p.is_delete = 'Y'
+	AND p.is_delete = 'N'
 ORDER BY priority, COALESCE(p.update_at, p.create_at) DESC;
 
 -- 3. 게시글 검색
@@ -44,7 +49,7 @@ FROM post p
 JOIN user u ON p.user_code = u.user_code
 JOIN post_category c ON p.category_id = c.category_id
 WHERE c.category_name = '자유게시판'
-	 AND p.is_delete = 'Y'
+	 AND p.is_delete = 'N'
     AND p.post_title LIKE CONCAT('%', '갓생', '%')  -- 검색 키워드
 ORDER BY COALESCE(p.update_at, p.create_at) DESC;
 
@@ -68,18 +73,60 @@ LEFT JOIN (
     GROUP BY post_id
 ) pl ON p.post_id = pl.post_id
 WHERE p.post_id = 3
-	 AND p.is_delete = 'Y';  -- 조회할 게시글 ID
-	 
+	 AND p.is_delete = 'N';  -- 조회할 게시글 ID
+
 -- 5. 게시글 추가
 INSERT INTO post (post_title, post_content, user_code, category_id)
 VALUES ('하마도 아니고 물 어케 2리터 마시냐', '저는 하마입니다', 11, 1);
+
+SELECT 
+    p.post_id AS 게시글ID, 
+    u.nickname AS 닉네임, 
+    p.post_title AS 제목,
+    p.post_content AS 내용, 
+    i.img_url AS 사진, 
+    COALESCE(p.update_at, p.create_at) AS 작성시간
+  FROM post p 
+  JOIN user u ON p.user_code = u.user_code
+  LEFT JOIN post_img i ON p.post_id = i.post_id
+ WHERE p.is_delete = 'N'
+ ORDER BY p.post_id DESC LIMIT 1;
+
+
 
 -- 6. 게시글 수정
 update post
 SET post_title = '하루에 물 2리터는 하마 아니냐?'
 WHERE post_id = 362;
 
-— 7. 게시글 삭제
+SELECT 
+    p.post_id AS 게시글ID, 
+    u.nickname AS 닉네임, 
+    p.post_title AS 제목,
+    p.post_content AS 내용, 
+    i.img_url AS 사진, 
+    COALESCE(p.update_at, p.create_at) AS 작성시간
+  FROM post p 
+  JOIN user u ON p.user_code = u.user_code
+  LEFT JOIN post_img i ON p.post_id = i.post_id
+ WHERE p.is_delete = 'N'
+ ORDER BY p.post_id DESC LIMIT 1;
+
+-- 7. 게시글 삭제
 UPDATE post 
-SET is_delete = 'Y'
-WHERE post_id = 362;
+   SET is_delete = 'Y'
+ WHERE post_id = 362;
+
+SELECT 
+    p.post_id AS 게시글ID, 
+    u.nickname AS 닉네임, 
+    p.post_title AS 제목,
+    p.post_content AS 내용, 
+    i.img_url AS 사진, 
+    COALESCE(p.update_at, p.create_at) AS 작성시간,
+    p.is_delete AS 삭제여부
+  FROM post p 
+  JOIN user u ON p.user_code = u.user_code
+  LEFT JOIN post_img i ON p.post_id = i.post_id
+ WHERE p.is_delete = 'Y'
+ ORDER BY p.post_id DESC LIMIT 1;
--- sql/community/review_board.sql
+++ sql/community/review_board.sql
@@ -13,7 +13,14 @@ WHERE c.category_name = '후기'
 	AND p.is_delete = 'N'
 ORDER BY 작성시간 DESC;
 
+
+
 -- 2. 팔로잉 우선 조회
+INSERT INTO follow (following_code, follower_code)
+VALUES 
+	(16, 2)
+ON DUPLICATE KEY UPDATE following_code = VALUES(following_code);	
+
 SELECT 
     p.post_id AS 게시글ID,
     p.post_title AS 제목,
@@ -27,10 +34,12 @@ FROM post p
 JOIN post_category c ON p.category_id = c.category_id
 JOIN user u ON p.user_code = u.user_code
 LEFT JOIN follow f ON p.user_code = f.following_code 
-    AND f.follower_code = 1  -- 사용자 코드 테스트용
+    AND f.following_code = 16  -- 사용자 코드 테스트용
 WHERE c.category_name = '후기'
 AND p.is_delete = 'N'
 ORDER BY priority, COALESCE(p.update_at, p.create_at) DESC;
+
+
 
 -- 3. 게시글 검색
 SELECT 
@@ -43,10 +52,42 @@ JOIN user u ON p.user_code = u.user_code
 JOIN post_category c ON p.category_id = c.category_id
 WHERE c.category_name = '후기'
 	 AND p.is_delete = 'N'
-    AND p.post_title LIKE CONCAT('%', '갓생', '%')  -- 검색 키워드
+    AND p.post_title LIKE CONCAT('%', '집밥', '%')  -- 검색 키워드
 ORDER BY COALESCE(p.update_at, p.create_at) DESC;
 
+
+
 -- 4. 게시글 상세 조회 
+SELECT 
+    p.post_id AS 게시글ID, 
+    u.nickname AS 닉네임, 
+    p.post_title AS 제목,
+    p.post_content AS 내용, 
+    i.img_url AS 사진, 
+    COALESCE(p.update_at, p.create_at) AS 작성시간,
+    os.opu_content AS OPU내용,
+    COALESCE(pl.좋아요수, 0) AS 좋아요수
+FROM post p 
+JOIN user u ON p.user_code = u.user_code
+LEFT JOIN opu_add oa ON p.opu_add_id = oa.opu_add_id
+LEFT JOIN opu_list ol ON oa.opu_list_id = ol.opu_list_id  
+LEFT JOIN opu_script os ON ol.opu_id = os.opu_id
+LEFT JOIN post_img i ON p.post_id = i.post_id
+LEFT JOIN (
+    SELECT 
+        post_id, 
+        COUNT(*) AS 좋아요수
+    FROM post_like
+    GROUP BY post_id
+) pl ON p.post_id = pl.post_id
+WHERE p.post_id = 4  
+  AND p.is_delete = 'N';
+
+
+
+-- 5. 게시글 추가  
+INSERT INTO post (post_title, post_content, user_code, category_id, opu_add_id)
+VALUES ('[후기] 건강한 식단 성공 후기', '-5kg 감량 성공', 15, 2, 3);
 
 SELECT 
     p.post_id AS 게시글ID, 
@@ -62,19 +103,53 @@ LEFT JOIN opu_add oa ON p.opu_add_id = oa.opu_add_id
 LEFT JOIN opu_list ol ON oa.opu_list_id = ol.opu_list_id  
 LEFT JOIN opu_script os ON ol.opu_id = os.opu_id
 LEFT JOIN post_img i ON p.post_id = i.post_id
-WHERE p.post_id = 4  
-  AND p.is_delete = 'Y';
+WHERE p.is_delete = 'N'
+ORDER BY p.post_id DESC LIMIT 1;
 
--- 5. 게시글 추가  
-INSERT INTO post (post_title, post_content, user_code, category_id)
-VALUES ('[후기] 하체운동 주3일한 후기', '-5kg 감량 성공', 15, 3);
+
 
 -- 6. 게시글 수정
 UPDATE post
 SET post_title = '[OPU 성공 후기] 집밥 도선생'
-WHERE post_id = 365;  
+WHERE post_id = 366;  
+
+SELECT 
+    p.post_id AS 게시글ID, 
+    u.nickname AS 닉네임, 
+    p.post_title AS 제목,
+    p.post_content AS 내용, 
+    i.img_url AS 사진, 
+    COALESCE(p.update_at, p.create_at) AS 작성시간,
+    os.opu_content AS OPU내용
+FROM post p 
+JOIN user u ON p.user_code = u.user_code
+LEFT JOIN opu_add oa ON p.opu_add_id = oa.opu_add_id
+LEFT JOIN opu_list ol ON oa.opu_list_id = ol.opu_list_id  
+LEFT JOIN opu_script os ON ol.opu_id = os.opu_id
+LEFT JOIN post_img i ON p.post_id = i.post_id
+WHERE p.is_delete = 'N'
+ORDER BY p.post_id DESC LIMIT 1;
 
 -- 7. 게시글 삭제
 UPDATE post 
 SET is_delete = 'Y'
-WHERE post_id = 365;
+WHERE post_id = 366;
+
+
+SELECT 
+    p.post_id AS 게시글ID, 
+    u.nickname AS 닉네임, 
+    p.post_title AS 제목,
+    p.post_content AS 내용, 
+    i.img_url AS 사진, 
+    COALESCE(p.update_at, p.create_at) AS 작성시간,
+    os.opu_content AS OPU내용,
+    p.is_delete AS 삭제여부
+  FROM post p 
+  JOIN user u ON p.user_code = u.user_code
+  LEFT JOIN post_img i ON p.post_id = i.post_id
+  LEFT JOIN opu_add oa ON p.opu_add_id = oa.opu_add_id
+  LEFT JOIN opu_list ol ON oa.opu_list_id = ol.opu_list_id  
+  LEFT JOIN opu_script os ON ol.opu_id = os.opu_id
+ WHERE p.is_delete = 'Y'
+ ORDER BY p.post_id DESC LIMIT 1;
