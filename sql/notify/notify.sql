286639339701a83887f4a38aff92fd4987bed7f1
meowdule <meowdule73@gmail.com>
2025-02-14 09:28:46 +0900

    커뮤니티 신고기능 관련 테스트케이스 이미지 첨부

---
+++ sql/notify/notify.sql
@@ -0,0 +1,15 @@
+-- 게시글 신고
+INSERT INTO notify
+VALUES
+(NULL, '후기글인데 자유게시판에 올라와있어요!', NULL, 3, 7, NULL); -- 게시글 신고
+
+SELECT * FROM notify;
+
+
+-- 댓글 신고
+INSERT INTO notify
+VALUES
+(NULL, '욕설 신고합니다.' , NULL, 10, 3, 2);  -- 댓글 신고
+
+
+SELECT * FROM notify;
